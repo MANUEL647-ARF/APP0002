07a7db98c41fa957828cd8d4d8caaf296ee018f4
MANUEL647-ARF <yepezromancesarmanuel@cetis50cdmx.com>
2024-09-10 20:35:07 -0600

    APP02 COMPLETADO

---
+++ APP02/APP02/APP02/app02/main.py
@@ -0,0 +1,53 @@
+import flet as ft
+from flet_core import alignment
+
+def main(page: ft.Page):
+    page.title="¿Me perdonas?"
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.bgcolor="blue"
+    
+    lbl1=ft.Text("¿Me perdonas?",
+                style=ft.TextStyle(size=40,weight="bold"))
+    
+    ing1=ft.Image(src="triste.png",width=200,height=200)
+
+    btnSi=ft.ElevatedButton("SI",
+                            color="green",
+                            width=100,
+                            height=50)
+    
+    btnNo=ft.ElevatedButton("No",
+                            color="red",
+                            width=100,
+                            height=50)
+    
+    def no(e):
+        btnSi.width+=30
+        btnNo.height+=10
+        page.update()
+    
+    def si(e):
+        ing1.src="Feliz.png"
+        page.update()
+    
+    
+    btnSi.on_click=si
+    btnNo.on_click=no
+    
+    page.add(
+        ft.Column(
+            [
+                lbl1,
+                ing1,
+                ft.Row([btnSi,btnNo],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=20
+        )
+    )
+
+ft.app(main)
